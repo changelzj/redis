865aec823758f8ec98ad404c5a866a2c130874cb
LiuZijian <lzj@123>
2019-03-23 22:59:45 +0800

    md

--- src/main/java/exam/TestSave.java
+++ src/main/java/exam/TestSave.java
@@ -3,14 +3,19 @@ package exam;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 
+import java.util.HashSet;
+
 public class TestSave {
 
     @Test
     public void test() {
-        Jedis jedis = new Jedis("127.0.0.1");
+        Jedis jedis = new Jedis("192.168.228.10",6379);
+
         jedis.ping();
         jedis.set("hello", "world");
         jedis.set("可爱", "晓璐");
+
+
     }
 }
 
